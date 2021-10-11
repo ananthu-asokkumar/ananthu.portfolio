1bc032e89aee4398143cc3ec9f3410427fa8996e
ananthu-asokkumar <ananthu_asokkumar@masai.school>
2021-10-11 23:08:12 +0530

    portfolio

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import styled from 'styled-components';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GithubIcon from '@material-ui/icons/GitHub';
+import YoutubeIcon from '@material-ui/icons/YouTube';
+// import Particle from '../Components/Particle';
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+
+function HomePage() {
+    return (
+      <HomePageStyled>
+        <div className="particle-con">
+          {/* <Particle /> */}
+        </div>
+        <div className="typography">
+          <h2>
+            Hi, I'm <span>Ananthu Asokkumar</span>
+          </h2>
+          <br />
+          <p>A MERN stack developer with the knack of developing frontend and backend applications</p>
+          <div className="icons">
+            <a
+              href="https://www.linkedin.com/in/ananthu-asokkumar-70143212b/"
+              className="icon i-facebook"
+            >
+              <LinkedInIcon />
+            </a>
+            <a
+              href="https://github.com/ananthu-asokkumar"
+              className="icon i-github"
+            >
+              <GithubIcon />
+            </a>
+            {/* <a href="https://codepen.io/pen/" className="icon i-youtube">
+                        <YoutubeIcon />
+                    </a> */}
+          </div>
+        </div>
+      </HomePageStyled>
+    );
+}
+
+const HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+   
+    .typography{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        width: 80%;
+
+        .icons{
+            display: flex;
+            justify-content: center;
+            margin-top: 1rem;
+            .icon{
+                border: 2px solid var(--border-color);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border-radius: 50%;
+                transition: all .4s ease-in-out;
+                cursor: pointer;
+                &:hover{
+                    border: 2px solid var(--primary-color);
+                    color: var(--primary-color);
+                }
+                &:not(:last-child){
+                    margin-right: 1rem;
+                }
+                svg{
+                    margin: .5rem;
+                }
+            }
+
+            .i-youtube{
+                &:hover{
+                    border: 2px solid red;
+                    color: red;
+                }
+            }
+            .i-github{
+                &:hover{
+                    border: 2px solid #5F4687;
+                    color: #5F4687;
+                }
+            }
+        }
+    }
+`;
+
+export default HomePage;
---
+++ src/data/Projects.js
@@ -0,0 +1,76 @@
+import css1 from '../img/portImages/cv_css.png';
+import css2 from '../img/portImages/ms.png';
+import react1 from '../img/portImages/react_styled.png';
+import img2 from '../img/portImages/maya-4.jpg';
+import img3 from '../img/portImages/maya-3.jpg';
+import onn from '../img/portImages/onnBikes.PNG';
+import ixigo from '../img/portImages/ixigo.PNG'
+const Projects = [
+  {
+    id: 1,
+    category: "CSS js html",
+    image: onn,
+    link1: "https://github.com/shivam-singh-au17/project_oonbikes",
+    link2: "https://www.google.com",
+    title: "onnbikes.com clone",
+    text: "A collaborative project where our goal was to clone the website onnbikes.com",
+    techStack: "HTML | CSS | JavaScript"
+  },
+  {
+    id: 2,
+    category: "React mongodb",
+    image: ixigo,
+    link1: "https://github.com/ananthu-asokkumar/ixigo-clone",
+    link2: "https://www.google.com",
+    title: "ixigo.com clone",
+    text: "Clone of an Indian online travel portal, which was achieved collaboratively in 5 days ",
+    techStack: "HTML | CSS | JavaScript | React | MongoDB"
+  },
+  // {
+  //     id: 3,
+  //     category: 'Javascript',
+  //     image: img3,
+  //     link1: 'https://www.google.com',
+  //     link2: 'https://www.google.com',
+  //     title: 'How To Animate In Maya',
+  //     text: 'Number one Animation Application'
+  // },
+  // {
+  //     id: 4,
+  //     category: 'Animation',
+  //     image: img2,
+  //     link1: 'https://www.google.com',
+  //     link2: 'https://www.google.com',
+  //     title: 'How To Use Blender',
+  //     text: 'Free Animation Software'
+  // },
+  // {
+  //     id: 5,
+  //     category: 'Animation',
+  //     image: img2,
+  //     link1: 'https://www.google.com',
+  //     link2: 'https://www.google.com',
+  //     title: 'How To Use Blender',
+  //     text: 'Free Animation Software'
+  // },
+  // {
+  //     id: 6,
+  //     category: 'React JS',
+  //     image: react1,
+  //     link1: 'https://www.google.com',
+  //     link2: 'https://www.google.com',
+  //     title: 'Responsive Portfolio Website',
+  //     text: 'Created using different technologies such as Material UI, Styled-Components and more...'
+  // },
+  // {
+  //     id: 7,
+  //     category: 'CSS',
+  //     image: css2,
+  //     link1: 'https://www.google.com',
+  //     link2: 'https://www.google.com',
+  //     title: 'Microsoft Website Clone',
+  //     text: 'Created using HTML and CSS'
+  // }
+];
+
+export default Projects;
